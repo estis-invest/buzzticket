863441739e3e92d3683783982713eef04aafc655
efpcode <efpjobhunt@gmail.com>
2026-04-21 10:48:31 +0200

    feat(domain): add UserRepository port for user persistence

---
+++ domain/src/main/java/com/efpcode/domain/user/port/UserRepository.java
@@ -0,0 +1,28 @@
+package com.efpcode.domain.user.port;
+
+import com.efpcode.domain.partner.model.PartnerId;
+import com.efpcode.domain.user.model.User;
+import com.efpcode.domain.user.model.UserCreatedAt;
+import com.efpcode.domain.user.model.UserEmail;
+import com.efpcode.domain.user.model.UserId;
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository {
+
+  Optional<User> findUserById(UserId id);
+
+  Optional<User> findUserByEmail(UserEmail email);
+
+  List<User> findByUserCreatedAtRange(UserCreatedAt startDate, UserCreatedAt endDate);
+
+  List<User> findAll();
+
+  List<User> findByPartnerId(PartnerId id);
+
+  boolean existsByEmail(UserEmail email);
+
+  void save(User user);
+
+  void deleteByUserId(UserId id);
+}
