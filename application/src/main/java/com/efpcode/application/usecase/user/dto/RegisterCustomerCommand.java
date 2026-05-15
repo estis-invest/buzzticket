9f79bf9a0f986b98ed2d603bc5cafa232574b2cb
efpcode <efpjobhunt@gmail.com>
2026-05-15 15:22:20 +0200

    fix: password raw text shown

--- application/src/main/java/com/efpcode/application/usecase/user/dto/RegisterCustomerCommand.java
+++ application/src/main/java/com/efpcode/application/usecase/user/dto/RegisterCustomerCommand.java
@@ -1,3 +1,12 @@
 package com.efpcode.application.usecase.user.dto;
 
-public record RegisterCustomerCommand(String name, String email, String password) {}
+public record RegisterCustomerCommand(String name, String email, String password) {
+    @Override
+    public String toString() {
+        return "RegisterCustomerCommand{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + "REDACTED" + '\'' +
+                '}';
+    }
+}
